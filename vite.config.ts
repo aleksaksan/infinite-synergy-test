a5b46e9360af1144b72b2179f0a455d1ac167688
Aleksan <sanyok.10rus@gmail.com>
2025-03-26 00:16:14 +0300

    tryed to fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.json'],
-  base: '/infinite-synergy-test/'
+  base: '/infinite-synergy-test'
 })
